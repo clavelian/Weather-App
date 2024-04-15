798604ff1cc25b62c4387c571f893dc3b678abf2
Clavelian <ianclavel1991@gmail.com>
2024-04-15 20:49:37 +0800

    zxc 1st commit

---
+++ zxc/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+// import Hello from './HelloWorld'
+// import LampDemo from './components/ui/lamp'
+import BackgroundGradient from './components/ui/background-gradient'
+
+function App() {
+
+
+  return (
+    <>
+      {/* <Hello />  */}
+      {/* <LampDemo /> */}
+      <BackgroundGradient className='w-96 h-96'/>
+    </>
+  )
+}
+
+export default App
