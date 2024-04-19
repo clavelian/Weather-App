965c84d831ca3ebaae064cb92325945a980e4c06
Clavelian <ianclavel1991@gmail.com>
2024-04-19 11:38:04 +0800

    feat: added correct folders

--- zxc/src/App.jsx
+++ zxc/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 // import Hello from './HelloWorld'
 // import LampDemo from './components/ui/lamp'
 // import BackgroundGradient from './components/ui/background-gradient'
-import BackgroundGradientDemo from './components/ui/BackgroundGradientDemo'
+import BackgroundGradientDemo from './pages/BackgroundGradientDemo'
 
 function App() {
 
