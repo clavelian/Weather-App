485ecabb481949b70b2e4b7bcb1bbc6707bc3aad
Clavelian <ianclavel1991@gmail.com>
2024-04-29 23:51:23 +0800

    feat: added sparkles bug: cannot add input and submit button

--- zxc/src/App.jsx
+++ zxc/src/App.jsx
@@ -2,18 +2,30 @@
 import './App.css'
 // import Hello from './HelloWorld'
 // import LampDemo from './components/ui/lamp'
-// import BackgroundGradient from './components/ui/background-gradient'
-import BackgroundGradientDemo from './components/ui/BackgroundGradientDemo'
-
+// import BackgroundGradientDemo from './components/ui/BackgroundGradientDemo'
+import SparklesPreview from './pages/SparklesDemo'
 function App() {
 
 
   return (
     <>
+    <SparklesPreview>
       {/* <Hello />  */}
       {/* <LampDemo /> */}
-      {/* <BackgroundGradient className='w-96 h-96'/> */}
-      <BackgroundGradientDemo />
+      {/* <BackgroundGradientDemo /> */}
+
+      </SparklesPreview>
+      <div>
+          <input
+            type="text"
+            placeholder="Type a city"
+            id="City"
+            className="appearance-none border-2 bg-slate-200 w-72 mt-4 px-2 mx-3 text-black"
+          />
+          <button type="submit" className="border-2 rounded-md">
+            Submit
+          </button>
+        </div>
     </>
   )
 }
--- zxc/src/components/ui/BackgroundGradientDemo.tsx
+++ zxc/src/components/ui/BackgroundGradientDemo.tsx
@@ -5,43 +5,34 @@ import BackgroundGradient from "./background-gradient";
 
 function BackgroundGradientDemo() {
   return (
-    <div>
-      <BackgroundGradient className="rounded-[20px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900">
-        {/* <Image
+    <>
+      <div>
+        <BackgroundGradient className="rounded-[20px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900">
+          {/* <Image
           src={`/jordans.webp`}
           alt="jordans"
           height="400"
           width="400"
           className="object-contain"
         /> */}
-        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
-          Air Jordan 4 Retro Reimagined
-        </p>
+          <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+            Air Jordan 4 Retro Reimagined
+          </p>
 
-        <p className="text-sm text-neutral-600 dark:text-neutral-400">
-          The Air Jordan 4 Retro Reimagined Bred will release on Saturday,
-          February 17, 2024. Your best opportunity to get these right now is by
-          entering raffles and waiting for the official releases.
-        </p>
-        <button className="rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800">
-          <span>Buy now </span>
-          <span className="bg-zinc-700 rounded-full text-[0.6rem] px-2 py-0 text-white">
-            $100
-          </span>
-        </button>
-      </BackgroundGradient>
-      <div>
-        <input
-          type="text"
-          placeholder="Type a city"
-          id="City"
-          className="appearance-none border-2 bg-slate-200 w-72 mt-4 px-2 mx-3 text-black"
-        />
-        <button type="submit" className="border-2 rounded-md">
-          Submit
-        </button>
+          <p className="text-sm text-neutral-600 dark:text-neutral-400">
+            The Air Jordan 4 Retro Reimagined Bred will release on Saturday,
+            February 17, 2024. Your best opportunity to get these right now is
+            by entering raffles and waiting for the official releases.
+          </p>
+          <button className="rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800">
+            <span>Buy now </span>
+            <span className="bg-zinc-700 rounded-full text-[0.6rem] px-2 py-0 text-white">
+              $100
+            </span>
+          </button>
+        </BackgroundGradient>
       </div>
-    </div>
+    </>
   );
 }
 export default BackgroundGradientDemo;
