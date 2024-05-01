9ae8b3c4bc9b2d0b04b3da5f0dd25009270a57f8
Clavelian <ianclavel1991@gmail.com>
2024-05-01 22:26:56 +0800

    Feat: added Background Beams

--- zxc/tailwind.config.js
+++ zxc/tailwind.config.js
@@ -1,4 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+ 
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+
 export default {
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -24,5 +32,15 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
