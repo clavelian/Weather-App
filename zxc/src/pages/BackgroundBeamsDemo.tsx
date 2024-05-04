f4abc3b2549990a08ef4b0ba0eb89c9031dd24b3
Kenneth Ian Clavel <ianclavel1991@gmail.com>
2024-05-04 23:32:48 +0800

    fix: export default BackgroundBeamsDemo

--- zxc/src/pages/BackgroundBeamsDemo.tsx
+++ zxc/src/pages/BackgroundBeamsDemo.tsx
@@ -1,8 +1,7 @@
-"use client";
 import React from "react";
 import { BackgroundBeams } from "../components/ui/background-beams";
 
-export function BackgroundBeamsDemo() {
+function BackgroundBeamsDemo() {
   return (
     <div className="h-[40rem] w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
       <div className="max-w-2xl mx-auto p-4">
@@ -27,3 +26,4 @@ export function BackgroundBeamsDemo() {
     </div>
   );
 }
+export default BackgroundBeamsDemo;
